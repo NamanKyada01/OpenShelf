0229da1a9a373eb1c04966f5ff4f5337781e35a7
callmenk36 <callmenk36@gmail.com>
2026-06-28 18:20:25 +0530

    Fix: bypass dummy Jest tests breaking CI pipeline

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native(-community)?|@react-navigation|react-redux|@op-engineering/op-sqlite|immer|@reduxjs/toolkit|react-native-worklets)',
+  ],
+  setupFiles: ['<rootDir>/jest.setup.js'],
 };
